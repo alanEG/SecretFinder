26b991843669dc2161118cd4c52802b9cd010e0e
abdallaEG <52584554+alanEG@users.noreply.github.com>
2021-05-14 05:07:12 +0200

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,4 +1,4 @@
-{
+_regex = {
     'google_api': r'AIza[0-9A-Za-z-_]{35}',
     'firebase'  : r'AAAA[A-Za-z0-9_-]{7}:[A-Za-z0-9_-]{140}',
     'google_captcha' : r'6L[0-9A-Za-z-_]{38}|^6[0-9a-zA-Z_-]{39}$',
